cb6d26599727152744ca431331148a4640752fe8
Ericson Matheus <45068732+ericsonmatheus@users.noreply.github.com>
2022-06-08 17:44:28 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Hello World
+        Em breve estára disponível o portfólio
       </header>
     </div>
   );
