6cb834694bf0be609ffbefca6e17de6f4305e7af
ericson matheus <ericson.matheus.2016@gmail.com>
2022-06-09 12:32:17 -0300

    index (refactor): add href to media social

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -190,9 +190,9 @@ function Index() {
           <p>Copyright &copy; 2020, ETECIA DS - Todos Direitos reservados</p>
           <div class="social-list">
             <ul>
-              <li><a href="/"><i class="fab fa-facebook"></i></a></li>
-              <li><a href="/"><i class="fab fa-github"></i></a></li>
-              <li><a href="/"><i class="fab fa-twitter"></i></a></li>
+              <li><a href="https://www.linkedin.com/in/ericson-matheus/"  target="_blank" rel="noreferrer"><i class="fab fa-linkedin"></i></a></li>
+              <li><a href="https://github.com/ericsonmatheus"  target="_blank" rel="noreferrer"><i class="fab fa-github"></i></a></li>
+              <li><a href="https://twitter.com/manosony21"  target="_blank" rel="noreferrer"><i class="fab fa-twitter"></i></a></li>
             </ul>
           </div>
         </div>
